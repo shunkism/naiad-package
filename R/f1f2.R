a2d5931a86e45c8d295ea7344461f0bb0cd16ae3
Shuntaro Koizumi <shunkism@gmail.com>
2024-10-08 14:50:06 +0200

    F1F2 Fix
    A little error with dealing with stations that are all NA, and the way dataframes were being joined.

--- R/f1f2.R
+++ R/f1f2.R
@@ -11,12 +11,12 @@
 
 f1f2 <- function(dataClean){
   F1 <- dplyr::group_by(dataClean, Station,River,Date)
-  F1 <- dplyr::inner_join(F1,F1F2.Weight, by = 'Species')
+  F1 <- dplyr::left_join(F1,F1F2.Weight, by = 'Species')
   F1 <- dplyr::mutate(F1, Value = as.numeric(Value))
   F1 <- dplyr::mutate(F1, F1 = Verdi)
   F1 <- dplyr::select(F1, -Verdi)
   F1 <- dplyr::group_by(F1, Station, Date)
-  F1 <- dplyr::mutate(F1, F1Station = max(F1))
+  F1 <- dplyr::mutate(F1, F1Station = ifelse(all(is.na(F1)), NA, max(F1, na.rm = TRUE)))
   
   F2 <- F1 %>% 
     dplyr::filter(Orden=="Plecoptera" | Orden=="Ephemeroptera") %>% 
@@ -27,7 +27,7 @@ f1f2 <- function(dataClean){
     tidyr::pivot_wider(names_from=Orden, values_from=totvalue, values_fill = 0) %>% # making ephem and pleco into two distinct columns instead of rows
     dplyr::mutate(F2=if_else(Plecoptera>0, true=0.5+(Ephemeroptera/Plecoptera), false=F1Station)) %>% 
     dplyr::select(River, Station, Date, F1Station,F2) %>% 
-#    dplyr::mutate(F2 = ifelse(F2 > 1.0, 1.0, F2)) %>% 
+    #    dplyr::mutate(F2 = ifelse(F2 > 1.0, 1.0, F2)) %>% 
     dplyr::select(River, Station, Date, F2)
   
   F1F2 <- F1 %>% 
