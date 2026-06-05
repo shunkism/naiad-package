1b51634352daed42ec4e5de5c951ba990bf123e6
shunkism <shunkism@gmail.com>
2026-06-05 14:48:47 +0200

    Add synonym handling and preserve originals
    Introduce a synonyms map and store original species names before cleaning.  Synonyms vector is extensible for adding more name pairs.

--- R/cleanTax.R
+++ R/cleanTax.R
@@ -12,28 +12,39 @@
 #' @export
 
 cleanTax <- function(datain) {
+    
+    # --- Synonym map: "old name" = "accepted name" ---
+    synonyms <- c(
+      "Limnaea glabra"     = "Omphiscola glabra",
+      "Lymnaea glabra"     = "Omphiscola glabra",
+      "Nigrobaetis niger"  = "Baetis niger"
+      # Add more pairs here as needed
+    )
+    
+    # Add a new column to store original species names
+    datain$Original_Species <- datain$Species
+    
+    # Clean up species names
+    datain$Species <- gsub("\\s+$", "", datain$Species)
+    datain$Species <- gsub("( indet\\.?| sp\\.?($|\\s))", "", datain$Species)
+    datain$Species <- gsub("\\s*/\\s*", "/", datain$Species)
+    
+    # --- Apply synonym replacement before fuzzy matching ---
+    matched_syn <- synonyms[datain$Species]
+    datain$Species <- ifelse(!is.na(matched_syn), matched_syn, datain$Species)
+    
+    # Fuzzy match against Taxonomy
+    correct_names <- stringdist::amatch(datain$Species, Taxonomy$Species, maxDist = 2)
+    
+    # Remove unmatched names and return a list of removed names
+    removed_names <- unique(datain$Species[is.na(correct_names)])
+    datain <- datain[!is.na(correct_names),]
+    
+    # Replace species names with corrected names
+    datain$Species <- Taxonomy$Species[correct_names[!is.na(correct_names)]]
+    datain <- datain[, !names(datain) %in% "Original_Species"]
+    
+    list2env(list(dataClean = datain, ErrorList = removed_names), envir = .GlobalEnv)
   
-  #Load in Taxonomy df
-
-  # Add a new column to store original species names
-  datain$Original_Species <- datain$Species
-  
-  # Clean up species names
-  datain$Species <- gsub("\\s+$", "", datain$Species) # remove extra spaces
-  datain$Species <- gsub("( indet\\.?| sp\\.?($|\\s))", "", datain$Species) # remove indet. or sp. at the end or followed by space
-  datain$Species <- gsub("\\s*/\\s*", "/", datain$Species) # remove spaces before and after slashes
-  
-  # Create a mapping of corrected names using amatch
-  correct_names <- stringdist::amatch(datain$Species, Taxonomy$Species, maxDist = 2)
-  
-  # Remove unmatched names and return a list of removed names
-  removed_names <- unique(datain$Species[is.na(correct_names)])
-  datain <- datain[!is.na(correct_names),]
-  
-  # Replace species names with corrected names
-  datain$Species <- Taxonomy$Species[correct_names[!is.na(correct_names)]]
-  datain <- datain[,! names(datain) %in% "Original_Species"]    
-  # Return the cleaned up dataframe and list of removed names
-  list2env(list(dataClean = datain, ErrorList = removed_names),envir = .GlobalEnv)
 }
 
